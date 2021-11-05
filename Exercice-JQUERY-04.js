40313040deb074f8f0d9b8a8249cbd7ec2d8433a
Sébastien Ferran <sebferran@gmail.com>
2021-11-05 14:50:17 +0100

    Ajout Exercice jQuery n°4

--- Exercice-JQUERY-04.js
+++ Exercice-JQUERY-04.js
@@ -9,4 +9,21 @@ Indice :
 Vous devrez créer 2 fonctions et 2 événements respectivement.
 */
 
+$(document).ready(function(){
 
+    var joueurencours = "X";
+
+    function changetour(){
+        $("#tour-joueur").toggleClass("rouge", "bleu");
+        $(this).html(joueurencours);
+        if(joueurencours == "X"){
+            joueurencours = "O";
+        }
+        else{
+            joueurencours = "X";
+        }
+    }
+
+    $("button").click(changetour);
+
+});
