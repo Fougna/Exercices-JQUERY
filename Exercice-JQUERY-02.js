88f568cff63c138a614c40ed109a56635a33ad71
Sébastien Ferran <sebferran@gmail.com>
2021-11-05 01:05:22 +0100

    Ajout exercice JQUERY n°2

--- Exercice-JQUERY-02.js
+++ Exercice-JQUERY-02.js
@@ -1,5 +1,5 @@
-/* 
-Projet n°2:
+/* Exercice jQuery n°2 (Conçu par Laurence Doe)
+
 Ecrivez un programme pour ouvrir et fermer un cadenas.
 Vous vous servirez des images suivantes :
 1. Cadenas fermé
@@ -11,3 +11,17 @@ Indice :
 Vous devrez écrire 2 fonctions et utiliser 2 évènements.
 */
 
+$(document).ready(function(){
+
+    function open(){
+        $("#cadenas").attr("src", "https://cdn-icons-png.flaticon.com/512/880/880779.png")
+    };
+
+    function close(){
+        $("#cadenas").attr("src", "https://cdn-icons-png.flaticon.com/512/841/841097.png")
+    }
+
+    $("#btn-open").click(open);
+    $("#btn-close").click(close);
+
+});
